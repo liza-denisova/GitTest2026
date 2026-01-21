7fa0248470137582dbf21b0a2c7a829098ff98e3
cpplv <89207657+cpplv@users.noreply.github.com>
2026-01-21 12:11:11 +0200

    Update pogas nosaukums

--- GitTest2026/Form1.Designer.cs
+++ GitTest2026/Form1.Designer.cs
@@ -65,7 +65,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "poga2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
